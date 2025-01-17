65aa5a48b8c6f3f5affb9e71765f34d5a64a6591
Ganon <100484118+xxreflextheone@users.noreply.github.com>
2025-01-17 12:53:52 -0500

    Added relative example

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import mouse
+import mouse as GHUB
+import win32api
 
-x, y = 200, 200 # sample coordinates to move the mouse to
+GHUB.mouse_open() # initialize mouse
+time.sleep(1)
 
-mouse.mouse_open() # initialize mouse
-mouse.mouse_move(0, x, y, 0) # move mouse to specified coords above.
+def enabled():
+  return win32api.GetKeyState(0x02) in (-127, -128) # 0x02 is right click. See https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+
+x, y = 1, 1 # sample coordinates to move the mouse to
+
+# Example usage
+while enabled():
+  mouse.mouse_move(0, x, y, 0) # move mouse to specified coords above.
